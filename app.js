4e95825e84e284ecf2ef5c8e1662265bfecd7a4a
vitorhensvv <vitorhenriqslv@gmail.com>
2020-11-11 10:27:40 -0300

    segundo commit

--- app.js
+++ app.js
@@ -2,15 +2,38 @@
 const express = require('express')
 //Instanciando o express
 const app = express();
+//Para tratativa de erro
+const morgan = require('morgan');
 //Rotas
 const rotaProdutos = require('./routes/produtos');
 const rotaPedido = require('./routes/pedido');
+
+//Setando ambiente no qual o morgan será utilizado
+//Executa um callback para monitorar as rotas
+app.use(morgan('dev')); 
 //Chamando a rota
 app.use('/produtos', rotaProdutos);
 app.use('/pedido', rotaPedido);
 
+//Tratamento de erro caso nenhuma das rotas acima seja chamada
+app.use((req, res, next) =>{
+    const erro = new Error('Rota não encontrada');
+    erro.status = 404;
+    next(erro);
+});
+
+//Caso o erro seja diferente de 404 é retornado 500
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
 //req = requisição, res = resposta, next = chamando outro método
-//utilizando rotas
+//Utilizando rotas
 // app.use('/teste', (req, res, next) =>{
 //  res.status(200).send({
 //      mensagem: "OK, deu certo"
